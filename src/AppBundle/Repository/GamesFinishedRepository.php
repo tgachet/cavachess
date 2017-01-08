32236b3da0a5f0c1a00f611f8fcb7b9bba6f1440
3olf <gachet.thomas@gmail.com>
2017-01-08 18:53:19 +0100

    Added ranking page

--- src/AppBundle/Repository/GamesFinishedRepository.php
+++ src/AppBundle/Repository/GamesFinishedRepository.php
@@ -10,23 +10,5 @@ namespace AppBundle\Repository;
  */
 class GamesFinishedRepository extends \Doctrine\ORM\EntityRepository
 {
-    public function findGamesWon($id){
-        $qb = $this->createQueryBuilder('gf')
-                ->innerJoin('gf.idwinner', 'u' )
-                ->where('gf.idwinner = :id')
-                ->setParameter('id', $id)
-        ;
-        
-        return $qb->getQuery()->getResult();
-    }
-    
-    public function findGamesLost($id){
-        $qb = $this->createQueryBuilder('gf')
-                ->innerJoin('gf.idlooser', 'u' )
-                ->where('gf.idlooser = :id')
-                ->setParameter('id', $id)
-        ;
-        
-        return $qb->getQuery()->getResult();
-    }
+
 }
