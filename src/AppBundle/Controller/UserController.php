b8f69e615c19a34daa4d371983c093eac3e1e07f
Unknown <ali-belqziz.esc@em-lyon.com>
2017-01-11 11:34:06 +0100

    fixed minor issues

--- src/AppBundle/Controller/UserController.php
+++ src/AppBundle/Controller/UserController.php
@@ -29,7 +29,7 @@ class UserController extends Controller
             $user = $user->setAllFriends(); 
         }
         
-        $user->setAllFriends();
+//        $user->setAllFriends();
         return $this->render(
             'user/list.html.twig',
             [
